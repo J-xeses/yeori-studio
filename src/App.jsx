1a9c93f54f1f0ea250392c0fb4a05902d84a02ff
J-xeses <won566800@gmail.com>
2026-05-03 21:10:52 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,43 +9,41 @@ import ExtractTab from './tabs/ExtractTab'
 import VideoTab from './tabs/VideoTab'
 import ThumbnailTab from './tabs/ThumbnailTab'
 import DashboardTab from './tabs/DashboardTab'
+import RetentionHookTab from './tabs/RetentionHookTab'
+import EditMetaTab from './tabs/EditMetaTab'
 import s from './App.module.css'
 
 const TAB_MAP = {
-  script: ScriptGenTab,
-  studio: StudioTab,
-  tts: TTSTab,
-  voice: VoiceTab,
-  extract: ExtractTab,
-  video: VideoTab,
-  thumbnail: ThumbnailTab,
-  dashboard: DashboardTab,
-  import RetentionHookTab from './tabs/RetentionHookTab'
-  import EditMetaTab      from './tabs/EditMetaTab'
-
-// TAB_MAP 안에
-retention: RetentionHookTab,
-editmeta:  EditMetaTab,
+    script: ScriptGenTab,
+    studio: StudioTab,
+    tts: TTSTab,
+    voice: VoiceTab,
+    extract: ExtractTab,
+    video: VideoTab,
+    thumbnail: ThumbnailTab,
+    dashboard: DashboardTab,
+    retention: RetentionHookTab,
+    editmeta: EditMetaTab,
 }
 
 function Layout() {
-  const { state } = useApp()
-  const Tab = TAB_MAP[state.activeTab] || ScriptGenTab
-  return (
-    <div className={s.app}>
-      <NavBar />
-      <ApiBar />
-      <div className={s.content}>
-        <Tab />
-      </div>
-    </div>
-  )
+    const { state } = useApp()
+    const Tab = TAB_MAP[state.activeTab] || ScriptGenTab
+    return (
+          <div className={s.app}>
+                  <NavBar />
+                  <ApiBar />
+                  <div className={s.content}>
+                            <Tab />
+                  </div>
+          </div>
+        )
 }
 
 export default function App() {
-  return (
-    <AppProvider>
-      <Layout />
-    </AppProvider>
-  )
-}
+    return (
+          <AppProvider>
+                <Layout />
+          </AppProvider>
+        )
+}</AppProvider>
